2b440656f3dbc48ede5430a1b6921b5ecb7f1dbc
onealv4846 <52715339+onealv4846@users.noreply.github.com>
2019-07-09 13:36:11 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_VincentOneal.py
@@ -0,0 +1,25 @@
+6-18-2019
+#This program is a Sales Prediction of annual sales
+
+#Pseudocode
+# 1.) Declare variables
+#     A brief description of the project
+#     Date
+#     CTI-110P2T1-Sales Prediction
+#     Your name
+
+# 2.) Prompt the user to determine company's annual profit
+# 3.) Enter the projected amount of total sales
+# 4.) Display the project that will be made from that amount
+
+#declare variable
+annual 
+projected 
+profit 
+
+
+
+
+
+
+
